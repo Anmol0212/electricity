ae5f822cd812dd7b0876a0ca66bd3a2c07569f21
Anmol varshney <56043213+Anmol0212@users.noreply.github.com>
2022-12-26 04:26:27 +0530

    Delete demo directory

--- demo/src/main/java/com/electricity/bill/repository/billRepository.java
+++
@@ -1,9 +0,0 @@
-package com.electricity.bill.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.electricity.bill.entity.bill;
-
-public interface billRepository extends JpaRepository<bill,Long>{
-
-}
